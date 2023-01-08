9843a94e4bb833cdd37d5a8ab61325259757e3bd
Jesse Hunter <jjsbizarreadventure@yahoo.com>
2023-01-08 10:25:50 -0600

    commit tree c36c088d25ae6ce02d1e722c1a22a62819b60b66

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = [];
+
+cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name) {
+   const lastCats = [...cats, name];
+   return lastCats;
+}
+
+function prependCat(name) {
+    const firstCats = [name, ...cats];
+    return firstCats;
+}
+
+function removeLastCat(removeLast) {
+    const cutLastCat = cats.slice(0, 2)
+    return cutLastCat
+}
+
+function removeFirstCat(removeFirst) {
+    const cutFirstCat = cats.slice(1)
+    return cutFirstCat
+}
